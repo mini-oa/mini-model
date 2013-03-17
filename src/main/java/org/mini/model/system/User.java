727d9f381e02a2293297ea761a18eadbfa3d755e
mini-oa <278245180@qq.com>
2013-03-17 22:03:10 +0800

    删除测试文件，增加用户表相关实体

---
+++ src/main/java/org/mini/model/system/User.java
@@ -0,0 +1,122 @@
+/**
+ * 
+ */
+package org.mini.model.system;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.Type;
+import org.mini.model.IdEntity;
+
+/**
+ * @author Administrator
+ * 
+ */
+@Entity
+@Table(name="mini_sys_user_info")
+public class User extends IdEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3636886603693310633L;
+
+	@Column(name = "user_name")
+	private String userName;
+	
+	@Column(name = "emp_no")
+	private String empNo;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "valid_flag")
+	@Type(type = "true_false")
+	private boolean validFlag;
+
+	@Column(name = "create_time")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createTime;
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName
+	 *            the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the empNo
+	 */
+	public String getEmpNo() {
+		return empNo;
+	}
+
+	/**
+	 * @param empNo
+	 *            the empNo to set
+	 */
+	public void setEmpNo(String empNo) {
+		this.empNo = empNo;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	/**
+	 * @return the validFlag
+	 */
+	public boolean isValidFlag() {
+		return validFlag;
+	}
+
+	/**
+	 * @param validFlag the validFlag to set
+	 */
+	public void setValidFlag(boolean validFlag) {
+		this.validFlag = validFlag;
+	}
+
+	/**
+	 * @return the createTiem
+	 */
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	/**
+	 * @param createTiem
+	 *            the createTiem to set
+	 */
+	public void setCreateTime(Date createTime) {
+		this.createTime= createTime;
+	}
+
+}
